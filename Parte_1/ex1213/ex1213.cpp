a590b2d7e321f27efcb87084106c6dbddc5a5f16
LFuciarelli <ratoratoso@gmail.com>
2020-10-12 12:32:47 +0200

    updates

--- Parte_1/ex1213/ex1213.cpp
+++ Parte_1/ex1213/ex1213.cpp
@@ -3,16 +3,11 @@
 using namespace std;
 
 int main(){
-	cout << "tre è maggiore di uno = ";
-	cout << boolalpha << (3 > 1);
-	cout << "\nquattro diviso due è minore di zero = ";
-	cout << boolalpha << ((4/2) < 0);
-	cout << "\nil carattere zero è uguale al valore zero = ";
-	cout << boolalpha << ('0' == 0);
-	cout << "\ndieci mezzi è compreso fra zero escluso e dieci incluso = ";
-	cout << boolalpha << (0 < (10/2)) and ((10/2) <= 10);
-	cout << "\nnon è vero che tre è magiore di due e minore di uno = ";
-	cout << boolalpha << not(3 > 2 and 3 < 1);
-	cout << "\ntre minore di meno cinque implica sette maggiore di zero = ";
-	cout << boolalpha << (not(3 < 5) or (7 > 0)) << endl;
+	cout << boolalpha;
+	cout << "tre è maggiore di uno = " << (3 > 1) << endl;
+	cout << "quattro diviso due è minore di zero = " << ((4/2) < 0) << endl;
+	cout << "il carattere zero è uguale al valore zero = " << ('0' == 0) << endl;
+	cout << "dieci mezzi è compreso fra zero escluso e dieci incluso = " << ((0 < (10/2)) and ((10.5) <= 10)) << endl;
+	cout << "non è vero che tre è magiore di due e minore di uno = " << (not(3 > 2 and 3 < 1)) << endl;
+	cout << "tre minore di meno cinque implica sette maggiore di zero = " << (not(3 < 5) or (7 > 0)) << endl;
 }
